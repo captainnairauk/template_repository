a1bf2f64d468ca3fc342ba5790922b29060b8e05
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2025-10-12 00:59:21 +0530

    Setup template

--- src/index.js
+++ src/index.js
@@ -1,28 +1,4 @@
 import "./style.css";
-import { createHomeTab } from "./home.js";
-import { createMenuTab } from "./menu.js";
-import { createAboutTab } from "./about.js";
+import { greeting } from "./greeting";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const contentDiv = document.getElementById("content");
-  const homeTabButton = document.getElementById("home-tab");
-  const menuTabButton = document.getElementById("menu-tab");
-  const aboutTabButton = document.getElementById("about-tab");
-
-  createHomeTab();
-
-  homeTabButton.addEventListener("click", () => {
-    contentDiv.innerHTML = "";
-    createHomeTab();
-  });
-
-  menuTabButton.addEventListener("click", () => {
-    contentDiv.innerHTML = "";
-    createMenuTab();
-  });
-
-  aboutTabButton.addEventListener("click", () => {
-    contentDiv.innerHTML = "";
-    createAboutTab();
-  });
-});
+console.log(greeting);
